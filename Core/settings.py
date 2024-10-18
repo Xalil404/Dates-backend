f293de01675b6a48b0e87a0f3c5d5b6225f4b623
Kareem <kareemkv1990@yahoo.com>
2024-10-18 14:58:36 +0100

    Added whitenoise

--- Core/settings.py
+++ Core/settings.py
@@ -108,6 +108,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Core.urls'
